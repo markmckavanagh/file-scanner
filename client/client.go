abcf7da892985ac8377eb06e383e2bcac8ed5257
markmckavanagh <mmckavanagh1@hotmail.com>
2025-02-22 15:08:12 +0000

    Updated client to use a goroutine per file when uploading to server

--- client/client.go
+++ client/client.go
@@ -2,15 +2,92 @@ package main
 
 import (
     "context"
-    "fmt"
     "io"
     "log"
     "os"
+    "path/filepath"
+    "sync"
 
 	"google.golang.org/grpc"
 
     pb "file-scanner/proto"
 )
+
+const chunkSize = 1024 * 1024 // 1MB per chunk
+
+func uploadFiles(client pb.FileServiceClient, rootDir string) {
+    var wg sync.WaitGroup
+
+    err := filepath.WalkDir(rootDir, func(path string, d os.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if !d.IsDir() {
+            wg.Add(1)
+            go uploadFile(client, path, &wg)
+        }
+
+        return nil
+    })
+
+    if err != nil {
+        log.Fatalf("Error walking directory: %v", err)
+    }
+
+    wg.Wait()
+}
+
+func uploadFile(client pb.FileServiceClient, filePath string, wg *sync.WaitGroup) {
+    defer wg.Done()
+
+    file, err := os.Open(filePath)
+    if err != nil {
+        log.Printf("Failed to open file %s: %v", filePath, err)
+        return
+    }
+    defer file.Close()
+
+    stream, err := client.UploadFile(context.Background())
+    if err != nil {
+        log.Fatalf("Failed to create stream: %v", err)
+    }
+
+    fileName := filepath.Base(filePath)
+    buffer := make([]byte, chunkSize)
+
+    for {
+        n, err := file.Read(buffer)
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            log.Printf("Error reading file %s: %v", filePath, err)
+            return
+        }
+
+        err = stream.Send(&pb.FileChunk{
+            FileName: fileName,
+            Content:  buffer[:n],
+        })
+        if err != nil {
+            log.Printf("Error sending chunk for file %s: %v", filePath, err)
+            return
+        }
+
+        log.Printf("Sent chunk of %d bytes for file %s", n, fileName)
+    }
+
+    res, err := stream.CloseAndRecv()
+    if err != nil {
+        log.Printf("Error closing stream for file %s: %v", fileName, err)
+        return
+    }
+
+    log.Printf("File '%s' uploaded successfully: %v", fileName, res.Message)
+}
+
+
 
 func main() {
     
@@ -22,40 +99,11 @@ func main() {
 
     client := pb.NewFileServiceClient(conn)
 
-    file, err := os.Open("testfile.txt")
-    if err != nil {
-        log.Fatalf("Failed to open file: %v", err)
-    }
-    defer file.Close()
+    rootDir := "./test_files"
 
-    stream, err := client.UploadFile(context.Background())
-    if err != nil {
-        log.Fatalf("Failed to create stream: %v", err)
-    }
+    log.Printf("Walking through directory %s", rootDir)
 
-    buffer := make([]byte, 1024)
-    for {
-        n, err := file.Read(buffer)
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            log.Fatalf("Failed to read file: %v", err)
-        }
+    uploadFiles(client, rootDir)
 
-        err = stream.Send(&pb.FileChunk{
-            FileName: "testfile.txt",
-            Content:  buffer[:n],
-        })
-        if err != nil {
-            log.Fatalf("Failed to send chunk: %v", err)
-        }
-    }
-
-    res, err := stream.CloseAndRecv()
-    if err != nil {
-        log.Fatalf("Failed to receive response: %v", err)
-    }
-
-    fmt.Printf("Upload status: Success=%v, Message=%s\n", res.Success, res.Message)
+    log.Println("All files uploaded successfully!")
 }
